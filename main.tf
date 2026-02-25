4613552ca56bf1bbc6c64a264e2fe928302c724c
Benoit Blais <benoit.blais@hashicorp.com>
2026-02-25 10:28:37 -0500

    fix: revert to data source with nonsensitive() - ephemeral outputs unsupported in root module

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 # Read a secret from a KV v2 secrets engine in HCP Vault
-ephemeral "vault_kv_secret_v2" "secret" {
+data "vault_kv_secret_v2" "secret" {
   mount = var.kv_mount
   name  = var.secret_path
 }
--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,6 @@
 output "secret_data" {
   description = "The key/value pairs retrieved from the KV v2 secret."
-  value       = nonsensitive(ephemeral.vault_kv_secret_v2.secret.data)
-  ephemeral   = true
+  value       = nonsensitive(data.vault_kv_secret_v2.secret.data)
 }
 
 # output "secret_metadata" {
