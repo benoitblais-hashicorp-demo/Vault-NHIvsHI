e2dbf8d795c2ebf54a8feee8589e51cd509d863a
Benoit Blais <benoit.blais@hashicorp.com>
2026-02-25 10:56:58 -0500

    chore: remove commented-out secret_metadata output

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,2 @@
 # Ephemeral values cannot be used in root module outputs (ephemeral outputs are not allowed).
 # The secret is displayed during apply via the terraform_data provisioner in main.tf.
-
-# output "secret_metadata" {
-#   description = "The metadata for the retrieved KV v2 secret (version, created_time, etc.)."
-#   value       = data.vault_kv_secret_v2.secret.metadata
-# }
