1daa77aef6b997b043616671a5a0e69ed7a4a095
Benoit Blais <benoit.blais@hashicorp.com>
2026-02-25 10:34:04 -0500

    fix: use ephemeral vault_kv_secret_v2 to suppress deprecation warning

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "secret_data" {
   description = "The key/value pairs retrieved from the KV v2 secret."
-  value       = nonsensitive(data.vault_kv_secret_v2.secret.data)
+  value       = nonsensitive(ephemeral.vault_kv_secret_v2.secret.data)
 }
 
 # output "secret_metadata" {
